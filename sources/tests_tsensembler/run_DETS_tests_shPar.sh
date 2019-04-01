bea738f7dd5551eb5cf709f349b1d7b67b3ad16f
Anonymous <>
2019-04-01 14:45:25 +0200

    adding tests tsensembler

--- sources/tests_tsensembler/run_DETS_tests_shPar.sh
+++ sources/tests_tsensembler/run_DETS_tests_shPar.sh
@@ -9,7 +9,7 @@ nbPar=$5;
 
 fun1 () {
   echo "Starting : run=$1 | day=$2 | nbModels=$3 | dataset=$4";
-  #R --slave -f run_DETS_tests_shPar.R --args indday=$2 nbmodels=$3 indrun=$1 dataset=$4;
+  R --slave -f run_DETS_tests_shPar.R --args indday=$2 nbmodels=$3 indrun=$1 dataset=$4;
   echo "Done : run=$1 | day=$2 | nbModels=$3 | dataset=$4";
 }
 export -f fun1; # export function so that it's inherited by subshells
